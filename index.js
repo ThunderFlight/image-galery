9a7499afc769f6de9bbffd23cfc5c9874324fbe6
ThunderFlight <igoreshkin2006@gmail.com>
2023-09-30 12:23:06 +0300

    feat: add button search and add dont find anything

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
-const main = document.querySelector('.main')
+const main = document.querySelector(".main");
 async function getDataGallary(value) {
-    main.replaceChildren()
-   const responce = await fetch(`https://api.unsplash.com/search/photos/?client_id=J00kjXQZqRy2E2NIokWLcTjGLN6SKIDJGfRtyuh4gug&query=${value}`)
-   const data = await responce.json()
-   
-   data.results.map((item)=>{
-        let img = document.createElement('img')
-        img.src=`${item.urls.full}`
-        img.className='galleryImage'
-        return(
-            main.appendChild(img)
-        )
-    })
-    console.log(document.querySelectorAll('.galleryImage'));
-    console.log(data);
-    return data
+  main.replaceChildren();
+  const responce = await fetch(
+    `https://api.unsplash.com/search/photos/?client_id=J00kjXQZqRy2E2NIokWLcTjGLN6SKIDJGfRtyuh4gug&query=${value}`
+  );
+  const data = await responce.json();
+
+  if (data.results.length === 0) {
+    let h1 = document.createElement("h1");
+    h1.innerText = ":( don`t find anything";
+    h1.style.color = "gray";
+    main.style.display='flex'
+    main.style.justifyContent='center'
+    main.style.alignItems='center'
+    main.style.height='65vh'
+    return main.appendChild(h1);
+  } else {
+    data.results.map((item) => {
+      let img = document.createElement("img");
+      img.src = `${item.urls.full}`;
+      img.className = "galleryImage";
+      return main.appendChild(img);
+    });
+  }
+  return data;
 }
-getDataGallary()
-document.querySelector('.searchImage').addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      getDataGallary(document.querySelector('.searchImage').value)
-    }
-}); 
+getDataGallary();
+document.querySelector(".searchImage").addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    getDataGallary(document.querySelector(".searchImage").value);
+  }
+});
+document
+  .querySelector(".searchImageButton")
+  .addEventListener("click", (event) => {
+    event.preventDefault();
+    getDataGallary(document.querySelector(".searchImage").value);
+  });
